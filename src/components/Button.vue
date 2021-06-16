2428d3995df8b6f14f14492a087b084f53c6d372
Protiva66 <jan.kaderka@seznam.cz>
2021-06-16 14:55:53 +0200

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :style="{background: color }" class="btn">
+      {{text}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button @click="onClick()" color="navy" text="Add task" />
+  </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {
+      Button 
+  },
+  methods:{
+      onClick(){
+          console.log('prdel');
+      }
+  }
+};
+
+</script>
